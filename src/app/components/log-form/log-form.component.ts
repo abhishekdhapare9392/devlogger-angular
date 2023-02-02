6e480a39c9a452ef5f81d44eba52522d656e93a0
Abhishek Dhapare <abhishek.dhapare@gmail.com>
2023-02-02 21:32:09 +0530

    logs application completed with localstorage

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -11,7 +11,9 @@ export class LogFormComponent implements OnInit {
 
   id!: string;
   text!: string;
-  date!: string;
+  date!: any;
+
+  isNew: boolean = true;
 
   constructor(private logService:LogService) { }
 
@@ -19,6 +21,7 @@ export class LogFormComponent implements OnInit {
     // Subscribe to the selected Log observable
     this.logService.selectLog.subscribe(log => { 
       if (log.id != null) { 
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
@@ -26,5 +29,44 @@ export class LogFormComponent implements OnInit {
     })
   }
 
+  onSubmit() { 
+    if (this.isNew) {
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      };
+      // add Log
+      this.logService.addLog(newLog);
+    } else { 
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+
+      // Update Log
+      this.logService.updateLog(updLog)
+    }
+
+    // Clear state
+    this.clearState();
+  }
+
+  clearState() { 
+    this.isNew = true;
+    this.id = '';
+    this.text = '';
+    this.date = '';
+    this.logService.clearState();
+  }
+
+  generateId() { 
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  }
 
 }
--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -17,20 +17,64 @@ export class LogService {
 
   selectLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() { 
-    this.logs = [
-      { id: '1', text: 'Generated Components', date: new Date('12/26/2022 12:54:23') },
-      { id: '2', text: 'Added Bootstrap', date: new Date('12/27/2022 09:35:23') },
-      {id: '3', text: 'Added logs components', date: new Date('12/27/2022 12:00:00')},
-    ]
+    // this.logs = [
+    //   { id: '1', text: 'Generated Components', date: new Date('12/26/2022 12:54:23') },
+    //   { id: '2', text: 'Added Bootstrap', date: new Date('12/27/2022 09:35:23') },
+    //   {id: '3', text: 'Added logs components', date: new Date('12/27/2022 12:00:00')},
+    // ]
+
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> { 
+    if (localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else { 
+      this.logs = JSON.parse(localStorage.getItem('logs') || '{}');
+    }
     return of(this.logs);
   }
 
   setFormLog(log: Log) { 
     this.logSource.next(log);
+  }
+
+  addLog(log: Log) { 
+    this.logs.unshift(log);
+
+    // Add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  updateLog(log: Log) { 
+    this.logs.forEach((cur, index) => { 
+      if (log.id === cur.id) { 
+        this.logs.splice(index, 1);
+      }
+    })
+    this.logs.unshift(log);
+
+    // Update to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  deleteLog(log: Log) { 
+    this.logs.forEach((cur, index) => { 
+      if (log.id === cur.id) { 
+        this.logs.splice(index, 1);
+      }
+    })
+
+    // Delete from local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
+  }
+
+  clearState() { 
+    this.stateSource.next(true);
   }
 
 }
